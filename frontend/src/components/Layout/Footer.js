622a38818946342c576a37fb00d7de30422dc387
Lalit9025 <lalit3chouhan@gmail.com>
2024-03-20 23:58:15 +0530

    frontend added

---
+++ frontend/src/components/Layout/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <h4 className='text-center '>All Right Reserved &copy; LalitYt</h4>
+        <p className='text-center mt-3'>
+          <Link to='/about'>About</Link>|
+          <Link to='/contact'>Contact</Link>|
+          <Link to='/policy'>Privacy Policy</Link>
+
+        </p>
+        <p className='text-center '>made with ❤️ by Lalit</p>
+    </div>
+  )
+}
+
+export default Footer
